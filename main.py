3840c7048a7acbfb54f9e5d5b9009c872bddb8c7
Shasank Periwal <shasank.periwal@lumiq.ai>
2025-03-19 17:24:43 +0530

    Router llm added

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os 
 import dotenv
 dotenv.load_dotenv()
+from langchain.tools import tool, Tool
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain import hub
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-# from langchain.chains.retrieval import create_retrieval_chain
-# from langchain_core.runnables import RunnablePassthrough
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
 from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
@@ -32,12 +30,12 @@ def main():
 
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-    agent_executor.invoke(
-        input={
-            "input": """generate and save in qr_code directory 10 QRcodes
-                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
-        }
-    )
+    # agent_executor.invoke(
+    #     input={
+    #         "input": """generate and save in qr_code directory 10 QRcodes
+    #                             that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    #     }
+    # )
 
     csv_agent = create_csv_agent(
         llm = ChatGoogleGenerativeAI(temperature= 0, model="gemini-2.0-flash"),
@@ -45,9 +43,36 @@ def main():
         verbose = True,
         allow_dangerous_code=True
     )
-    csv_agent.invoke(input={"input": "How many columns are there in file episode_info.csv"})
-    csv_agent.invoke(input={"input": "In the file episode_info, which writer wrote the most episodes? How many episodes did he write? Split the writers if there are two or more writers in the same episode."})
+    # csv_agent.invoke(input={"input": "How many columns are there in file episode_info.csv"})
+    # csv_agent.invoke(input={"input": "In the file episode_info, which writer wrote the most episodes? How many episodes did he write? Split the writers if there are two or more writers in the same episode."})
 
+    # ROUTER GRAND AGENT
+
+    tools = [
+        Tool(
+            name="Python Agent",
+            func=agent_executor.invoke,
+            description="""useful when you need to transform natural language to python and execute the python code,
+                            returning the results of code execution
+                            DOES NOT ACCEPT CODE AS INPUT"""
+        ),
+        Tool(
+            name="CSV Agent",
+            func=csv_agent.invoke,
+            description="""useful when you need to answer question over episode_info.csv file,
+                            takes an input the entire question and returns the answer after running pandas calculation"""
+        )
+    ]
+
+    prompt = base_prompt.partial(instructions="")
+    grand_agent = create_react_agent(
+        prompt=prompt,
+        llm=ChatGoogleGenerativeAI(temperature= 0, model="gemini-2.0-flash"),
+        tools=tools
+    )
+    grand_agent_executor = AgentExecutor(agent=grand_agent, tools=tools, verbose=True)
+    # print(grand_agent_executor.invoke(input={"input": "Which season has the most episode?"}))
+    print(grand_agent_executor.invoke(input={"input": "Generate 12 QR code that point to instagram.com/shasankperiwal, you have qrcode package installed already"}))
 
 if __name__ == "__main__":
     main()
